8e0d60404a98591dfe86d3f78a5e778156e17fe1
zhangliang <1006981838@qq.com>
2025-03-06 15:16:40 +0800

    0

---
+++ h5/html/pc/js/base.js
@@ -0,0 +1,12 @@
+var h5Base="zb.tv189.com";
+var tomcatBase = "apizb.tv189.com";
+var webBaseUrl = "http://apizb.tv189.com";
+//本地服务
+//var h5Base="192.168.222.156:80";
+//var tomcatBase = "192.168.222.156:8080";
+//var webBaseUrl = "http://192.168.222.156:8080";
+
+//测试服务
+// var h5Base="zbt.tv189.net";
+// var tomcatBase = "apizbt.tv189.net";
+// var webBaseUrl = "http://apizbt.tv189.net";
